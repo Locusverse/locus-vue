040b65d5b504b51179b65219e873b234c2a8b6b2
Femi Obadimu <ficazzosam@gmail.com>
2026-03-24 10:13:09 +0100

    Frontend - Homepage - Completed

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,26 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import prerender from '@prerenderer/rollup-plugin'
+import PuppeteerRenderer from '@prerenderer/renderer-puppeteer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    tailwindcss(),
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    prerender({
+      routes: ['/'],
+      renderer: new PuppeteerRenderer({
+        renderAfterTime: 3000,
+      }),
+      postProcess(renderedRoute) {
+        renderedRoute.html = renderedRoute.html.replace(/data-server-rendered="true"/, '')
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
